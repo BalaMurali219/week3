f2fc20204c4a4e0d172fd3492af3c3c9e19f8f3e
BalaMurali219 <158822404+BalaMurali219@users.noreply.github.com>
2024-03-20 21:53:24 +0530

    Create overinher.java

---
+++ overinher.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class AddTwoNumbers{
+    int a,b,c;
+    void sum(int a,int b)
+    {
+        System.out.println(a+b);
+    }
+    
+};
+class AddThreeNumbers extends AddTwoNumbers{
+    void sum(int a,int b,int c)
+    {
+        System.out.println(a+b+c);
+    }
+
+};
+
+public class Overinher {
+    public static void main(String[] args) {
+        AddThreeNumbers obj = new AddThreeNumbers();
+        Scanner input = new Scanner(System.in);
+        obj.a= input.nextInt();
+        obj.b= input.nextInt();
+        obj.c= input.nextInt();
+        obj.sum(obj.a,obj.b,obj.c);
+        obj.sum(obj.a,obj.b);
+        input.close();
+
+
+    }
+}    
+
